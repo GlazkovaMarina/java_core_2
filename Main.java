ad8e316c8e27ad1dea9656ded0d742d42f424c54
GlazkovaMarina <glazkova.mv@bk.ru>
2024-01-02 15:12:52 +0300

    task1

---
+++ Main.java
@@ -0,0 +1,31 @@
+/**
+ * Main
+ * 1 Написать метод, возвращающий количество чётных элементов массива.
+ * countEvens([2, 1, 2, 3, 4]) → 3 countEvens([2, 2, 0]) → 3 countEvens([1, 3,
+ * 5]) → 0
+ * 2 Написать функцию, возвращающую разницу между самым большим и самым ма-
+ * леньким элементами переданного не пустого массива.
+ * 3 Написать функцию, возвращающую истину, если в переданном массиве есть два
+ * соседних элемента, с нулевым значением.
+ */
+public class Main {
+
+    public static int countEvens(int[] array) {
+        int count = 0;
+        for (int elem : array) {
+            if (elem % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = { 2, 1, 2, 3, 4 };
+        int[] arr2 = { 2, 2, 0 };
+        int[] arr3 = { 1, 3, 5 };
+        System.out.printf("%d\n", countEvens(arr1));
+        System.out.printf("%d\n", countEvens(arr2));
+        System.out.printf("%d\n", countEvens(arr3));
+    }
+}
